651428f65da0160480625a14e41202a1682739ed
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2022-12-19 16:26:41 +0300

    Add Project Files

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Melih Yuvacı on 19.12.2022.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
